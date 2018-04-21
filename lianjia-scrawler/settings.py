8fd3c66358220efb18a8aa40d1ebbab0a9f63322
choloe <twinkling_lights@outlook.com>
2018-04-21 21:05:59 +0800

    4/21

---
+++ lianjia-scrawler/settings.py
@@ -0,0 +1,9 @@
+DBENGINE = 'mysql' #ENGINE OPTIONS: mysql, sqlite3, postgresql
+DBNAME = 'lianjia'
+DBUSER = 'root'
+DBPASSWORD = '123456'
+DBHOST = '127.0.0.1'
+DBPORT = 3306
+CITY = 'sh' # only one, shanghai=sh shenzhen=sh......
+REGIONLIST = [u'minhang','baoshan','xuhui','putuo','yangpu','changning','songjiang','jiading',
+              'huangpu','jingan','zhabei','hongkou','qingpu','fengxian','jinshan','chongming','shanghaizhoubian'] # only pinyin support
