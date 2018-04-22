c9d3b2629b4c12f56b484babe90cfa823ae95e1a
wangyan127 <2603023634@qq.com>
2018-04-23 00:43:46 +0800

    create Django project juju

---
+++ lianjia-scrawler-master/lianjia/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\course\github\juju.com\lianjia-scrawler-master\lianjia\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
